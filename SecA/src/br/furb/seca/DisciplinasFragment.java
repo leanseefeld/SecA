d444b176e1ee1de0970ace3f14fc6921582b9955
Gustavo Sabel <gustavo.sabel.gs@gmail.com>
2014-11-03 00:09:28 -0200

    Montando a tela de Disciplinas

--- SecA/src/br/furb/seca/DisciplinasFragment.java
+++ SecA/src/br/furb/seca/DisciplinasFragment.java
@@ -1,7 +1,24 @@
 package br.furb.seca;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
 public class DisciplinasFragment extends MyFragment {
 
+	private String[] from = {"disciplina", "professor"};
+	private int[] to = { android.R.id.text1, android.R.id.text2 };
+	private ListView listDisciplinas;
+	
 	public DisciplinasFragment() {
 		super(R.layout.fragment_dashboard);
 	}
@@ -10,4 +27,44 @@ public class DisciplinasFragment extends MyFragment {
 		super(R.layout.fragment_disciplinas, sectionNumber);
 	}
 
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View v = super.onCreateView(inflater, container, savedInstanceState);
+		
+		listDisciplinas = (ListView)v.findViewById(R.id.disciplinas);
+		
+		atualizaDisciplinas(v.getContext());
+		
+		return v;
+	}
+	
+	public void atualizaDisciplinas(Context c)
+	{
+		SimpleAdapter adapter = new SimpleAdapter(
+				c, 
+				listarDisciplinas(), 
+				android.R.layout.simple_list_item_2, 
+				from, to);
+		
+		//Ta dando pau nessa p#$# quando vira atela :)
+		listDisciplinas.setAdapter(adapter);
+	}
+	
+	public List<Map<String, String>> listarDisciplinas()
+	{
+		List<Map<String, String>> disciplinas = new ArrayList<Map<String,String>>();
+		
+		Map<String, String> m = new HashMap<String,String>();
+		m.put("disciplina", "Sistemas Distribuidos");
+		m.put("professor", "Paulo");
+		disciplinas.add(m);
+		
+		m = new HashMap<String,String>();
+		m.put("disciplina", "Processo de Software I");
+		m.put("professor", "Everaldo");
+		disciplinas.add(m);
+		
+		return disciplinas;
+	}
 }
