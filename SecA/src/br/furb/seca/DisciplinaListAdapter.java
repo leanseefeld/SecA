d0a851f99a3ba7ab05e77b7d7dd103bffaabc6a5
leanseefeld <leanseefeld@gmail.com>
2014-11-07 01:57:28 -0200

    Tela Disciplinas: item da lista

--- SecA/src/br/furb/seca/DisciplinaListAdapter.java
+++ SecA/src/br/furb/seca/DisciplinaListAdapter.java
@@ -1,7 +1,6 @@
 package br.furb.seca;
 
 import java.util.List;
-
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,7 +14,7 @@ public class DisciplinaListAdapter extends ArrayAdapter<String[]> {
     private final List<String[]> disciplinas;
 
     public DisciplinaListAdapter(Context context, int resource, List<String[]> disciplinas) {
-	super(context, resource);
+	super(context, resource, disciplinas);
 	this.disciplinas = disciplinas;
 	inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
@@ -33,12 +32,12 @@ public class DisciplinaListAdapter extends ArrayAdapter<String[]> {
     public View getCustomView(int position, View converView, ViewGroup parent) {
 	View row = inflater.inflate(R.layout.fragment_disciplina_item, parent, false);
 	String[] disciplina = disciplinas.get(position);
-	TextView txtDisciplina = (TextView) row.findViewById(R.id.txtDisciplina);
+	TextView txtDisciplina = (TextView) row.findViewById(R.id.frDisciplinas_item_txtDisciplina);
 	txtDisciplina.setText(disciplina[0]);
-	TextView txtProfessor = (TextView) row.findViewById(R.id.txtProfessor);
+	TextView txtProfessor = (TextView) row.findViewById(R.id.frDisciplinas_item_txtProfessor);
 	txtProfessor.setText(disciplina[1]);
-	TextView txtNota = (TextView) row.findViewById(R.id.txtNota);
-	txtNota.setText(disciplina[3]);
+	TextView txtNota = (TextView) row.findViewById(R.id.frDisciplinas_item_txtNota);
+	txtNota.setText(disciplina[2]);
 	return row;
     }
 
--- SecA/src/br/furb/seca/DisciplinasFragment.java
+++ SecA/src/br/furb/seca/DisciplinasFragment.java
@@ -1,26 +1,23 @@
 package br.furb.seca;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
-import android.widget.SimpleAdapter;
 
 public class DisciplinasFragment extends MyFragment {
 
-    private String[] from = { "disciplina", "professor" };
-    private int[] to = { android.R.id.text1, android.R.id.text2 };
     private ListView listDisciplinas;
 
     public DisciplinasFragment() {
-		super(R.layout.fragment_disciplinas);
+	super(R.layout.fragment_disciplinas);
     }
 
     public DisciplinasFragment(int sectionNumber) {
@@ -31,7 +28,7 @@ public class DisciplinasFragment extends MyFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 	View v = super.onCreateView(inflater, container, savedInstanceState);
 
-	listDisciplinas = (ListView) v.findViewById(R.id.disciplinas_lista);
+	listDisciplinas = (ListView) v.findViewById(R.id.frDisciplinas_lista);
 
 	atualizaDisciplinas(v.getContext());
 
@@ -39,12 +36,21 @@ public class DisciplinasFragment extends MyFragment {
     }
 
     public void atualizaDisciplinas(Context c) {
-	SimpleAdapter adapter = new SimpleAdapter(c, listarDisciplinas(), android.R.layout.simple_list_item_2, from, to);
-
+	DisciplinaListAdapter adapter = new DisciplinaListAdapter(c, R.layout.fragment_disciplina_item, Arrays.asList(
+		new String[] { "Sistemas Distribuídos", "Paulo Fernando da Silva", "8.33" }, // 
+		new String[] { "Processo de Software I", "Everaldo Artur Grahl", "9.5" }, //
+		new String[] { "Desafios Sociais e Contemporâneos", "Cíntia Aparecida da Luz Silva Pereira Goes",
+			"10.0" }, //
+		new String[] { "Álgebra Linear para Computação", "Evandro Felin Londero", "2.45" }, //
+		new String[] { "Linguagens de Programação", "José Roque Voltolini da Silva", "7.0" }, //
+		new String[] { "Teoria dos Grafos", "Aurélio Hoppe", "8.0" }, //
+		new String[] { "Arquitetura de Computadores", "Miguel Alexandre Wisintainer", "7.77" }, //
+		new String[] { "Linguagens Formais", "Joyce Martins", "8.9" } //
+		));
 	listDisciplinas.setAdapter(adapter);
     }
 
-    public List<Map<String, String>> listarDisciplinas() {
+    private List<Map<String, String>> listarDisciplinas() {
 	List<Map<String, String>> disciplinas = new ArrayList<Map<String, String>>();
 
 	Map<String, String> m = new HashMap<String, String>();
